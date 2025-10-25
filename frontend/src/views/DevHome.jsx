22def7f92889c91ff83eb166ec377f8948416233
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-10-24 21:43:38 -0500

    Added dev page 'DevHome.jsx' to test http request to backend

---
+++ frontend/src/views/DevHome.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function DevHome() {
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async function getPlayers() {
+    try {
+      const resp = await axios.get("http://127.0.0.1:8000/api/players/");
+      console.log(resp.data);
+      setPlayers(resp.data);
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
+
+  function renderPlayers(playersList) {
+    return playersList.map((player) => {
+        const playerName = player.name;
+      return <div>{playerName}</div>;
+    });
+  }
+
+  return (
+    <>
+      <div>{players.map((player) => {
+            return <div>{player.name}</div>
+          })}</div>
+      <div>{error ?? error}</div>
+    </>
+  );
+}
