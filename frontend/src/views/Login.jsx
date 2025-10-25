4bfe67352a19c147ad242537fdaaae11b3a48a98
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-10-25 14:29:47 -0500

    Login component now sets AuthContext's tokens upon success

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,58 +1,70 @@
 import {useState} from "react";
 import axios from "axios";
+import {useAuth} from "../context/AuthContext.jsx";
+import {useNavigate} from "react-router";
 
 export default function Login() {
     const [username, setUsername] = useState(null);
     const [password, setPassword] = useState(null);
-    const [access, setAccess] = useState(null);
-    const [refresh, setRefresh] = useState(null);
+    const [loginSuccess, setLoginSuccess] = useState(false);
     const [error, setError] = useState(null);
+    const {login} = useAuth();
 
-    async function login(e) {
+    const navigate = useNavigate();
+
+    async function getTokenPair(e) {
         e.preventDefault();
         try {
             const resp = await axios.post("http://127.0.0.1:8000/api/token/", {username, password});
             console.log(resp);
             if (resp.status === 200) {
-                setAccess(resp.data["access"]);
-                setRefresh(resp.data["refresh"]);
+                setLoginSuccess(true);
+                login({
+                    access: resp.data["access"],
+                    refresh: resp.data["refresh"],
+                })
             }
         } catch (err) {
             console.error(err);
             setError(err.response?.data?.detail || err.response?.data?.non_field_errors?.[0] || 'Invalid Credentials');
+            setLoginSuccess(false);
         }
     };
 
+    const LoginSuccessMessage = () => {
+        return <div>
+            Login successful. If you are not automatically redirect, click
+            <a onClick={() => navigate("/")}>here</a>
+        </div>
+    }
+
     return (<div>
-            <h3>LOG IN</h3>
-            {error ?? <p>{error}</p>}
-            <form onSubmit={(e) => login(e)}>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button>
-                    SUBMIT
-                </button>
-            </form>
-            {access ?? (<div style={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
-                    <p><strong>Access:</strong>{access}</p>
-                    <p><strong>Refresh:</strong>{refresh}</p>
-                </div>)}
-        </div>)
+        <h3>LOG IN</h3>
+        {error ?? <p>{error}</p>}
+        <form onSubmit={(e) => getTokenPair(e)}>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+            </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+            <button>
+                SUBMIT
+            </button>
+        </form>
+        {loginSuccess ?? <LoginSuccessMessage/>}
+    </div>)
 
 }
