7d6b6462f36063dea9b5c9e026a54835a74759ee
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-10-31 15:31:23 -0500

    Implemented TableList component in RaidAttendanceListView.jsx

--- frontend/src/views/RaidAttendanceListView.jsx
+++ frontend/src/views/RaidAttendanceListView.jsx
@@ -1,5 +1,5 @@
 import {useRaidAttendanceList} from "../hooks/requests.js";
-import {getRaRows, getRaTable} from "./utils.jsx";
+import {getCell, getLinkCell, TableList} from "../components/Tables.jsx";
 
 export function RaidAttendanceListView() {
     const {data: raData, isPending, error} = useRaidAttendanceList();
@@ -7,10 +7,17 @@ export function RaidAttendanceListView() {
     if (isPending) return <>LOADING...</>;
     if (error) return <>{error.message}</>
 
-    const raRows = getRaRows(raData);
-    const raTable = getRaTable(raRows);
+    const getRaCells = (data) => {
+        return data.map((row) => {
+            return [
+                getLinkCell(row?.raid?.name, `/raid/${row?.raid?.id}`),
+                getCell(row?.raid.zone?.name),
+                getLinkCell(row?.player?.name, `/player/${row?.player?.id}`),
+                getCell(row?.created_at),
+            ]
+        });
+    }
 
-    return <>
-        {raTable}
-    </>
+    const headers = ["Raid", "Zone", "Player", "Date"];
+    return <TableList headers={headers} reducedData={getRaCells(raData)}/>
 }
