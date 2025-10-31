b0438b443a3d1242034ed7cbc6113ce327309bab
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-10-30 20:00:53 -0500

    Minor CSS cleanup

--- frontend/src/views/itemAwardedListView.jsx
+++ frontend/src/views/itemAwardedListView.jsx
@@ -35,55 +35,54 @@ export function ItemAwardedListView() {
 
     const table = (rows) => {
         return (
-            <Table >
+            <Table>
                 <TableHead>
-                <TableRow>
-                    <TableCell id="table-header">
-                    </TableCell>
-                    <TableCell id="table-header">
-                        Name
-                    </TableCell>
-                    <TableCell id="table-header">
-                        Player
-                    </TableCell>
-                    <TableCell id="table-header">
-                        Raid
-                    </TableCell>
-                    <TableCell id="table-header">
-                        Date
-                    </TableCell>
-                </TableRow>
+                    <TableRow>
+                        <TableCell id="table-header">
+                        </TableCell>
+                        <TableCell id="table-header">
+                            Name
+                        </TableCell>
+                        <TableCell id="table-header">
+                            Player
+                        </TableCell>
+                        <TableCell id="table-header">
+                            Raid
+                        </TableCell>
+                        <TableCell id="table-header">
+                            Date
+                        </TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
-                {rows.map((row) => {
-                    return (
-                        <TableRow>
-                            <TableCell id="clickable-cell">
-                                <img
-                                    id="item-icon"
-                                    src={getIconPath(row.iconId)}
-                                    alt={row.name}
-                                />
-                                {row.icon}
-                            </TableCell>
-                            <TableCell id="clickable-cell">
-                                {row.name}
-                            </TableCell>
-                            <TableCell id="clickable-cell" onClick={(_) => handleClick("players", row.playerId)}>
-                                {row.player}
-                            </TableCell>
-                            <TableCell id="clickable-cell" onClick={(_) => handleClick("raids", row.raidId)}>
-                                {row.raid}
-                            </TableCell>
-                            <TableCell id="clickable-cell">
-                                {row.date}
-                            </TableCell>
-
-                        </TableRow>
-                    )
-                })}
+                    {rows.map((row) => {
+                        return (
+                            <TableRow id="table-row">
+                                <TableCell id="clickable-cell">
+                                    <img
+                                        id="item-icon"
+                                        src={getIconPath(row.iconId)}
+                                        alt={row.name}
+                                    />
+                                    {row.icon}
+                                </TableCell>
+                                <TableCell id="clickable-cell">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell id="clickable-cell" onClick={(_) => handleClick("players", row.playerId)}>
+                                    {row.player}
+                                </TableCell>
+                                <TableCell id="clickable-cell" onClick={(_) => handleClick("raids", row.raidId)}>
+                                    {row.raid}
+                                </TableCell>
+                                <TableCell id="non-clickable-cell">
+                                    {row.date}
+                                </TableCell>
+                            </TableRow>
+                        )
+                    })}
                 </TableBody>
-            </Table >
+            </Table>
         )
     };
 
