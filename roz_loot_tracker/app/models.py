5c9678299a65d5da9bd53e4d8e058f2610554621
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-01 11:36:34 -0500

    Added unique constraint to name field for Player model

--- roz_loot_tracker/app/models.py
+++ roz_loot_tracker/app/models.py
@@ -32,7 +32,7 @@ class Zone(models.Model):
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
