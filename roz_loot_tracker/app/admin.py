258a2661c82e2a4ff9bec68f77257e0b33e4a891
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-02 19:21:45 -0600

    Added inline form to admin raid class

--- roz_loot_tracker/app/admin.py
+++ roz_loot_tracker/app/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
-from .models import Player, Raid, RaidAttendance, Character, ItemAwarded, Item, PreferredPixel
+from .models import Player, Raid, RaidAttendance, Character, ItemAwarded, Item, PreferredPixel, Zone
+
 
 admin.site.site_header = "RoZ Raid Tracker Admin Terminal"
-
-
-admin.site.register(Player)
-admin.site.register(Raid)
 admin.site.register(RaidAttendance)
 admin.site.register(Character)
+
+
+class ItemAwardedInline(admin.TabularInline):
+    model = ItemAwarded
+    fields = ['player', 'item']
+    autocomplete_fields = ['player', 'item']
+
+    def get_queryset(self, request):
+        return self.model.objects.none()
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        formset.max_num = 50
+        formset.extra = 1
+        return formset
+
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
 
 
 @admin.register(Item)
@@ -15,11 +32,27 @@ class ItemAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
-@admin.register(ItemAwarded)
-class ItemAwardedAdmin(admin.ModelAdmin):
-    autocomplete_fields = ('item',)
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
 
 
 @admin.register(PreferredPixel)
 class PreferredPixelAdmin(admin.ModelAdmin):
     autocomplete_fields = ('item',)
+
+
+@admin.register(Raid)
+class RaidAdmin(admin.ModelAdmin):
+    inlines = [ItemAwardedInline]
+    list_display = ['name', 'zone', 'created_at']
+    search_fields = ['name']
+    autocomplete_fields = ('zone',)
+
+
+@admin.register(ItemAwarded)
+class ItemAwardedAdmin(admin.ModelAdmin):
+    list_display = ['item', 'player', 'raid', 'created_at']
+    list_filter = ['created_at', 'raid__name']
+    search_fields = ['item__name', 'player__name', 'raid__name']
+    autocomplete_fields = ['item', 'player', 'raid']
