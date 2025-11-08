fd290497e70d9fccfade309e4bd9d32c89c01676
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-07 21:14:52 -0600

    Wrapped App + Navbar in MessageProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,16 @@ import App from './App.jsx'
 import {NavBar} from "./components/NavBar.jsx";
 import {BrowserRouter} from "react-router";
 import {AuthProvider} from "./context/AuthContext.jsx";
+import {MessageProvider} from "./context/MessageContext.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <AuthProvider>
-                <NavBar/>
-                <App/>
+                <MessageProvider>
+                    <NavBar/>
+                    <App/>
+                </MessageProvider>
             </AuthProvider>
         </BrowserRouter>
     </StrictMode>
