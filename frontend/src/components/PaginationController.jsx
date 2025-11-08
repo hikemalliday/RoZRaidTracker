8586198aa037b7356c3da0e3eddf7cfda7e5ba20
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-08 15:37:34 -0600

    Added PaginationController.jsx

---
+++ frontend/src/components/PaginationController.jsx
@@ -0,0 +1,23 @@
+import { Button, Container } from '@mui/material';
+
+export function PaginationController({ setPage, previous, next, styles = {} }) {
+    const leftCarrot = '<';
+    const rightCarrot = '>';
+
+    return (
+        <Container sx={styles}>
+            <Button
+                sx={{ color: 'white' }}
+                onClick={_ => (previous ? setPage(prev => prev - 1) : null)}
+            >
+                {leftCarrot}
+            </Button>
+            <Button
+                sx={{ color: 'white' }}
+                onClick={_ => (next ? setPage(prev => prev + 1) : null)}
+            >
+                {rightCarrot}
+            </Button>
+        </Container>
+    );
+}
