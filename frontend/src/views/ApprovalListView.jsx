e74fc6da3ccc9d6644e3e1b3ecd5ae3f87a4f41e
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-08 18:11:25 -0600

    Updated ApprovalListView to handle paginated response

--- frontend/src/views/ApprovalListView.jsx
+++ frontend/src/views/ApprovalListView.jsx
@@ -1,11 +1,11 @@
-import {useRaidAttendanceApprovalList} from "../hooks/requests.js";
-import {ApprovalListTable} from "../components/ApprovalListTable.jsx";
-
+import { useRaidAttendanceApprovalList } from '../hooks/requests.js';
+import { ApprovalListTable } from '../components/ApprovalListTable.jsx';
 
 export function ApprovalListView() {
-    const {isPending, data, error} = useRaidAttendanceApprovalList({is_approved: false});
+    const { isPending, data, error } = useRaidAttendanceApprovalList({ is_approved: false });
 
     if (isPending) return <>LOADING...</>;
+    if (error) return <>{error.message}</>;
 
-    return <ApprovalListTable data={data}/>
+    return <ApprovalListTable data={data.results} />;
 }
