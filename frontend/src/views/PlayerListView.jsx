2b59e2219c72ec72c4d44ed8a5764816437bc58b
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-09 10:14:52 -0600

    Refactored itemAwardedListView.jsx and PlayerListView.jsx to use paginated hooks

--- frontend/src/views/PlayerListView.jsx
+++ frontend/src/views/PlayerListView.jsx
@@ -1,11 +1,11 @@
-import { usePlayerList } from '../hooks/requests.js';
+import { usePlayerListPaginated } from '../hooks/requests.js';
 import { PlayerListTable } from '../components/PlayerListTable.jsx';
 import { PaginatedListTable } from '../components/PaginatedListTable.jsx';
 
 export function PlayerListView() {
     return (
         <PaginatedListTable
-            requestHook={usePlayerList}
+            requestHook={usePlayerListPaginated}
             TableComponent={PlayerListTable}
             sortChoices={['name', 'lifetime_ra']}
         />
--- frontend/src/views/itemAwardedListView.jsx
+++ frontend/src/views/itemAwardedListView.jsx
@@ -1,11 +1,11 @@
-import { useItemAwardedList } from '../hooks/requests.js';
+import { useItemAwardedListPaginated } from '../hooks/requests.js';
 import { ItemAwardedListTable } from '../components/ItemAwardedListTable.jsx';
 import { PaginatedListTable } from '../components/PaginatedListTable.jsx';
 
 export function ItemAwardedListView() {
     return (
         <PaginatedListTable
-            requestHook={useItemAwardedList}
+            requestHook={useItemAwardedListPaginated}
             TableComponent={ItemAwardedListTable}
             sortChoices={['name', 'player', 'raid', 'date']}
         />
