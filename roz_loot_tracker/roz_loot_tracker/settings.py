416d25862bdbd151566eeda355a6e5530d36afc4
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-10 21:16:13 -0600

    Added ALLOWED_HOSTS_CONST

--- roz_loot_tracker/roz_loot_tracker/settings.py
+++ roz_loot_tracker/roz_loot_tracker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from .config import ALLOWED_HOSTS_CONST
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-@)umsv@bgc)x=nls84^=d32vcxnnobtbzz(sy=b#5xoc)m&=ap
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS_CONST
 
 
 # Application definition
