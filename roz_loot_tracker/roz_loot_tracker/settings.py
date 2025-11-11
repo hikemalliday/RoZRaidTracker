b91a4ff4718571951a4d0220cd1c2054442e86f6
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-10 22:18:23 -0600

    Added 'base' to vite config

--- roz_loot_tracker/roz_loot_tracker/settings.py
+++ roz_loot_tracker/roz_loot_tracker/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Django looks here when collectstatic is ran
 STATICFILES_DIRS = [BASE_DIR / 'app' / 'static']
+# Server servers from here in prod
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
