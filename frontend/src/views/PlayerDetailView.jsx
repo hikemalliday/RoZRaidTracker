daaef49ca00afa1ea83a868e9bebef011b561071
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-23 11:17:11 -0600

    Added Raid Attendance List Table to PlayerDetailView

--- frontend/src/views/PlayerDetailView.jsx
+++ frontend/src/views/PlayerDetailView.jsx
@@ -4,6 +4,7 @@ import { Container, Typography } from '@mui/material';
 import { renderErrors } from './utils.jsx';
 import { ItemAwardedListTable } from '../components/ItemAwardedListTable.jsx';
 import { CharacterListTable } from '../components/CharacterListTable.jsx';
+import { RaidAttendanceListTable } from '../components/RaidAttendanceListTable.jsx';
 
 export function PlayerDetailView() {
     const { id } = useParams();
@@ -23,10 +24,25 @@ export function PlayerDetailView() {
         error: characterError,
     } = useList('characters', '/characters/', { player: id });
 
-    if (isPlayerPending || isItemAwardedPending || isCharacterPending) return <>LOADING...</>;
+    const {
+        isPending: isRaidsPending,
+        data: raidsData,
+        error: raidsError,
+    } = useList('raid_attendance', '/raid_attendance/', { player: id });
 
-    const errorList = [playerError, itemAwardedError, characterError];
+    if (isPlayerPending || isItemAwardedPending || isCharacterPending || isRaidsPending)
+        return <>LOADING...</>;
+
+    const errorList = [playerError, itemAwardedError, characterError, raidsError];
     if (errorList.some(Boolean)) return renderErrors(errorList);
+
+    const _sortRaData = raData => {
+        return raData.sort((a, b) => {
+            const valA = a.created_at;
+            const valB = b.created_at;
+            return valB.localeCompare(valA);
+        });
+    };
 
     return (
         <Container>
@@ -42,6 +58,12 @@ export function PlayerDetailView() {
                     Items Awarded - Total: {itemAwardedData.count}
                 </Typography>
                 <ItemAwardedListTable data={itemAwardedData.results} sortable />
+            </Container>
+            <Container>
+                <Typography sx={{ mt: 5 }} variant="h6">
+                    Raids Attended: {itemAwardedData.count}
+                </Typography>
+                <RaidAttendanceListTable data={_sortRaData(raidsData?.results)} sortable />
             </Container>
             <Container sx={{ mt: 9 }}>
                 <Typography variant="h6">Characters</Typography>
