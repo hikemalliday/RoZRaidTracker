d30f4cf2eb7b6b012b0649b2649f77ef8c69c12f
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-12-28 10:39:48 -0600

    Added checkbox to select and delete raid attendance approval rows in list view

--- frontend/src/components/ApprovalPendingListTable.jsx
+++ frontend/src/components/ApprovalPendingListTable.jsx
@@ -1,8 +1,40 @@
 import { TableRow } from '@mui/material';
-import { getLinkCell, TableList } from './Tables.jsx';
+import { getCheckboxCell, getLinkCell, TableList } from './Tables.jsx';
 import { joinAndTruncate } from '../views/utils.jsx';
+import { useEffect, useState } from 'react';
+import { useRaidAttendanceApprovalDelete } from '../hooks/requests.js';
 
 export function ApprovalPendingListTable({ data }) {
+    const [raidsToDelete, setRaidsToDelete] = useState(new Set());
+    const { mutate } = useRaidAttendanceApprovalDelete();
+
+    useEffect(() => {
+        const raidIds = new Set(
+            data.map(raid => {
+                return raid.id;
+            })
+        );
+        setRaidsToDelete(raidIds);
+    }, [data]);
+
+    const handleCheckboxClick = (e, id) => {
+        const raidsSet = new Set(raidsToDelete);
+        const isChecked = e.target.checked;
+        if (isChecked) raidsSet.add(id);
+        else raidsSet.delete(id);
+        setRaidsToDelete(raidsSet);
+    };
+
+    const handleDeleteSelectedRaidsToApprove = async _ => {
+        if (raidsToDelete.size === 0) return;
+        const raidIdsArray = [...raidsToDelete];
+        const promises = raidIdsArray.map(p => {
+            return mutate(p);
+        });
+        await Promise.allSettled(promises);
+        setRaidsToDelete(new Set());
+    };
+
     const getApprovalRows = sorted => {
         return sorted.map((row, i) => {
             const detailRoute = `/ra_approval_pending/${row?.id}`;
@@ -12,6 +44,7 @@ export function ApprovalPendingListTable({ data }) {
                     {getLinkCell(row?.raid_name, detailRoute)}
                     {getLinkCell(joinAndTruncate(row?.players_list, 100), detailRoute)}
                     {getLinkCell(row?.created_at, detailRoute)}
+                    {getCheckboxCell(e => handleCheckboxClick(e, row?.id))}
                 </TableRow>
             );
         });
@@ -22,7 +55,22 @@ export function ApprovalPendingListTable({ data }) {
         'Raid Name': 'raid_name',
         Players: 'players_list',
         Date: 'created_at',
+        Remove: null,
     };
 
-    return <TableList data={data} getTableRows={getApprovalRows} headerMap={headerMap} />;
+    return (
+        <>
+            <TableList data={data} getTableRows={getApprovalRows} headerMap={headerMap} />
+            <button
+                style={{
+                    display: 'flex',
+                    alignItems: 'left',
+                    marginTop: 5,
+                }}
+                onClick={handleDeleteSelectedRaidsToApprove}
+            >
+                Remove Selected Raids
+            </button>
+        </>
+    );
 }
