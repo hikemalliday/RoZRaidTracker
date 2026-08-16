d5ee2b64f502ac4197eaa7b1c1b0e4a45b2a72f1
hikemalliday <hikemalliday@gmail.com>
2026-08-16 11:04:03 -0500

    refactored 'migrate_character_types' command

--- roz_loot_tracker/app/management/commands/migrate_character_types.py
+++ roz_loot_tracker/app/management/commands/migrate_character_types.py
@@ -13,17 +13,45 @@ def _get_char_type(character):
     return "ALT"
 
 
+def _validate_data_integrity(character):
+    """We want to detect rows that somehow have both "main" and "main_alt" bools"""
+    if character.is_main and character.is_main_alt:
+        return False
+    return True
+
+
+def _save_all_as_alt(characters):
+    """
+    In order to avoid edge case of issues related to DB level 'MAIN' constraint, we need to convert all characters to 'ALT' first.
+    Shouldn't normally come up, but I was messing with some things by hand and screw up some data before running this actual script.
+    """
+    for character in characters:
+        character.type = "ALT"
+        character.save()
+
+
 class Command(BaseCommand):
     help = "update character rows with type field"
-    with transaction.atomic():
-        def handle(self, *args, **options):
-            players = Player.objects.all()
+
+    def handle(self, *args, **options):
+        with transaction.atomic():
+            players = models.Player.objects.all()
             counter = 0
+            invalid_rows = []
             for player in players:
-                characters = player.characters
+                characters = player.characters.all()
+                _save_all_as_alt(characters)
                 for character in characters:
+                    is_char_valid = _validate_data_integrity(character)
+                    if not is_char_valid:
+                        invalid_rows.append(character.id)
+                        continue
                     char_type = _get_char_type(character)
                     character.type = char_type
                     character.save()
                     counter += 1
-            return self.stdout.write(f"Character rows updated: {counter}")
+            if invalid_rows:
+                self.stdout.write(self.style.WARNING(f"Some rows have been found to contain both 'main' and 'main_alt' as true. IDs:"))
+                for row_id in invalid_rows:
+                    self.stdout.write(str(row_id))
+        return self.stdout.write(f"Character rows updated: {str(counter)}")
