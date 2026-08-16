8481227e68a1972b44832bc894671a3427891b82
hikemalliday <hikemalliday@gmail.com>
2026-08-16 11:53:37 -0500

    PlayerListTable now shows main alt 1 and main alt 2

--- frontend/src/components/PlayerListTable.jsx
+++ frontend/src/components/PlayerListTable.jsx
@@ -1,16 +1,19 @@
 import { getCell, getLinkCell, TableList } from './Tables.jsx';
 import { TableRow } from '@mui/material';
-// TODO: There were mentions of allowing 2 main alts. So this single 'alt' might be insufficient
+
 export function PlayerListTable({ data, rowStyles = {}, sortable = true }) {
     const getPlayerRows = sorted => {
         return sorted.map((row, i) => {
             const main = row.characters.find(char => char.type === "MAIN");
-            const alt = row.characters.find(char => char.type === "MAIN_ALT");
+            const mainAlts = row.characters.filter(char => char.type === "MAIN_ALT");
+            const mainAlt1 = mainAlts.length > 0 ? mainAlts[0] : undefined;
+            const mainAlt2 = mainAlts.length > 1 ? mainAlts[1] : undefined;
             return (
                 <TableRow key={i} sx={rowStyles}>
                     {getLinkCell(row?.name, `/player/${row?.id}`)}
                     {getCell(main?.name, `/character/${main?.id}`)}
-                    {getCell(alt?.name, `/character/${alt?.id}`)}
+                    {getCell(mainAlt1?.name, `/character/${mainAlt1?.id}`)}
+                    {getCell(mainAlt2?.name, `/character/${mainAlt2?.id}`)}
                     {getCell(`${row?.lifetime_ra}%`)}
                     {getCell(`${row?.ra_21_day}%`)}
                     {getCell(`${row?.active}`)}
@@ -22,7 +25,8 @@ export function PlayerListTable({ data, rowStyles = {}, sortable = true }) {
     const headerMap = {
         Name: 'name',
         Main: null,
-        Alt: null,
+        'Main Alt 1': null,
+        'Main Alt 2': null,
         'Lifetime RA': 'lifetime_ra',
         '21 Day': 'ra_21_day',
         Active: 'active',
