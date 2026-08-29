dba30e2af1154b39bac5e335ee18824be4ea5dcd
hikemalliday <hikemalliday@gmail.com>
2026-08-29 11:48:07 -0500

    added command `set_item_tier_to_null.py`, added `prettier.config.js`. Not scoped to this branch name, but forgot to commit these awhile ago.

---
+++ frontend/prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+export default {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    printWidth: 120,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+};
---
+++ roz_loot_tracker/app/management/commands/set_item_tier_to_null.py
@@ -0,0 +1,17 @@
+import csv
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from app import models
+
+# I think we added this as a means to 'redo' a failed item tier migration
+class Command(BaseCommand):
+    help = "update item rows with tier as null"
+    with transaction.atomic():
+        def handle(self, *args, **options):
+            counter = 0
+            tierd = models.Item.objects.exclude(tier=None)
+            for item in tierd:
+                item.tier = None
+                item.save()
+                counter += 1
+            return self.stdout.write(f"Item rows updated: {counter}")
